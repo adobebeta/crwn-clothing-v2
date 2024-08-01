7b1161106e1516e055dbaf8927b02cb8d9a956c8
nussaba.k <adobebeta1997@gmail.com>
2024-08-01 16:45:21 +0700

    init e-commerce website

---
+++ src/components/catagory-item/catagory-item.component.jsx
@@ -0,0 +1,22 @@
+import './catagory-item.styles.scss'
+const CategoryItem = ({ category }) => {
+  const { title, imageUrl } = category;
+
+  console.log("imageUrl = ",imageUrl)
+
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ 
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+export default CategoryItem;
